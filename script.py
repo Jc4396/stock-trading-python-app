52ce1c9613f32a324ddde8f216569a2286728e2f
Jc4396 <jucaheag@google.com>
2025-10-01 20:09:19 -0600

    polygon

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import requests
 from dotenv import load_dotenv
@@ -6,8 +7,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
+
 LIMIT = 1000
 
 url = f"https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}"
 response = requests.get(url)
-print(response.json())
+tickers = []
+
+data = response.json()
+for ticker in data["results"]:
+    tickers.append(ticker)
+
+while "next_url" in data:
+    response = requests.get(data["next_url"] + f"&apiKey={POLYGON_API_KEY}")
+    data = response.json()
+    print(data)
+    for ticker in data["results"]:
+        tickers.append(ticker)
+    time.sleep(30)  # 5 requests per minute
+
+print(len(tickers))
